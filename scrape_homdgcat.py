666614db4b458ce21d09cc2b6f23ee8b05b142ca
chungyi.bossi <chungyi.bossi@gmail.com>
2025-08-29 15:27:35 +0800

    [feature] 取得卡片細節網址

--- scrape_homdgcat.py
+++ scrape_homdgcat.py
@@ -18,3 +18,7 @@ WebDriverWait(driver, 10).until(
 # five_stars_cards = driver.find_element(By.XPATH, "//div[contains(@class,'avatar-card') and contains(@class,'rar-5')]")
 cards = driver.find_elements(By.CLASS_NAME,'avatar-card')
 print(len(cards))
+card_links = list()
+for card in cards:
+    href = card.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    card_links.append(href) # 收集連結
